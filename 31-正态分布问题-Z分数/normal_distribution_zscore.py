44b47d8cd20181bb1573f2df6f98f0c0680a8142
zenghang-feng <60545562+zenghang-feng@users.noreply.github.com>
2024-08-29 18:12:02 +0800

    first version

---
+++ 31-正态分布问题-Z分数/normal_distribution_zscore.py
@@ -0,0 +1,14 @@
+# 假设有均值为81，标准差为6.3的正态分布
+mean = 81
+std = 6.3
+
+# Z-score：距离均值有多少个标准差的距离
+# 可以尝试不同的x取值
+x = 65
+z_score = (x-mean) / std
+print("Z分数的取值是：", z_score)
+
+# 可以尝试不同的x取值
+x = 83
+z_score = (x-mean) / std
+print("Z分数的取值是：", z_score)
